c622be9f4e99ad402753059f5ffa4081e16e5c41
LiuStar2233 <Lhx14776409450@163.com>
2024-07-12 14:29:45 +0800

    StarClient Alpha 1.0.2

---
+++ src/cn/star/client/mod/mods/render/NoHurtCameraMod.java
@@ -0,0 +1,20 @@
+package cn.star.client.mod.mods.render;
+
+import cn.star.client.mod.Mod;
+import cn.star.client.mod.mods.Category;
+import org.lwjgl.input.Keyboard;
+
+/**
+ * Created by IntelliJ IDEA
+ *
+ * @author LiuStar2233
+ * {@code @create} 2024/7/12 下午2:08
+ */
+
+public class NoHurtCameraMod extends Mod{
+    public NoHurtCameraMod() {
+        super("NoHurtCamera", Category.RENDER);
+        setKey(Keyboard.KEY_NONE);
+        setKeyboard("NONE");
+    }
+}
